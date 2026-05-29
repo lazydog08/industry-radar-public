6735eb88ad02ab0c07246487b8b42ab9fd8fcb38
lazydog <lazydog@local.lan>
2026-05-30 03:55:27 +0800

    Remove public GitHub Pages site

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { runReport, generatePeriodReport } from "./report/generate.js";
-export { Store } from "./store/db.js";
-export { loadConfig } from "./config.js";
--- src/scoring/score.ts
+++
@@ -1,1 +0,0 @@
-export { scoreItem } from "./radar.js";
